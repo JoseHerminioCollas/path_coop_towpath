beeca2444d643d4a8d7affbfb86460015b745f10
Jose Herminio Collas <goatstone@gmail.com>
2025-10-20 14:56:20 +0000

    WIP

--- gpx-kml-converter/scripts/run.sh
+++ gpx-kml-converter/scripts/run.sh
@@ -9,9 +9,11 @@ if [ ! -d "node_modules" ]; then
 fi
 
 echo "1) Update GPX with image <link> and <desc> tags"
-npx ts-node --esm src/update-gpx.ts
+# npx ts-node --esm src/update-gpx.ts
+# gpsbabel -i gpx -f coop_towpath_trkpt.gpx -o kml -F coop_towpath_trkpt.kml
+node  --trace-deprecation --loader  ts-node/esm src/update-gpx.ts
+# npx tsx src/update-gpx.ts
+# echo "2) Convert updated GPX → KML"
+# npx ts-node --esm src/convert-gpx-to-kml.ts
 
-echo "2) Convert updated GPX → KML"
-npx ts-node --esm src/convert-gpx-to-kml.ts
-
-echo "Done. You can now upload the generated KML (see src/upload-kml.ts for helper)."
+# echo "Done. You can now upload the generated KML (see src/upload-kml.ts for helper)."
