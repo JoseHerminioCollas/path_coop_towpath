fce14299d4ed51dcf097ef3789376474d551a6f4
Jose Herminio Collas <goatstone@gmail.com>
2025-10-20 16:15:17 +0000

    WIP

--- gpx-kml-converter/scripts/run.sh
+++ gpx-kml-converter/scripts/run.sh
@@ -9,8 +9,8 @@ if [ ! -d "node_modules" ]; then
 fi
 
 npx tsx src/update-gpx.ts
-gpsbabel -i gpx -f coop_towpath_wpt.gpx -o kml -F coop_towpath_wpt.kml 
-
+# gpsbabel -i gpx -f coop_towpath_wpt.gpx -o kml -F coop_towpath_wpt.kml 
+npx tsx src/convert-gpx-to-kml.ts
 echo "1) Update GPX with image <link> and <desc> tags"
 # npx ts-node --esm src/update-gpx.ts
 # gpsbabel -i gpx -f coop_towpath_trkpt.gpx -o kml -F coop_towpath_trkpt.kml
